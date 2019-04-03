537e2928c0d8ba3fe8037214185ef9e65a935b6a
Milena L. Seeburger <milena.seeburger@mail.de>
2019-04-03 17:11:50 +0200

    Sending POST to RequestBin

--- src/main/java/com/nexo/server/Service/NexoService.java
+++ src/main/java/com/nexo/server/Service/NexoService.java
@@ -3,7 +3,12 @@ package com.nexo.server.Service;
 import com.nexo.server.DAO.NexoDAO;
 import com.nexo.server.Entity.Tightening_Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,12 +19,21 @@ public class NexoService {
     private NexoDAO nexoDAO;
 
     public void insertResult(Tightening_Result tightening_result) {
+        requestBin(tightening_result);
         nexoDAO.insert(tightening_result);
-        System.out.println("Nr.: "+tightening_result.getNr());
-        System.out.println("IdCode: "+tightening_result.getIdCode());
+        System.out.println("Nr.: " + tightening_result.getNr());
+        System.out.println("IdCode: " + tightening_result.getIdCode());
     }
 
     public List<Tightening_Result> getAllResults() {
         return nexoDAO.findAll();
+    }
+
+    private void requestBin(Tightening_Result tightening_result) {
+        final String uri = "http://requestbin.fullcontact.com/112m0yb1";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<Tightening_Result> request = new HttpEntity<>(tightening_result);
+        restTemplate.postForEntity(uri, request, null);
     }
 }
